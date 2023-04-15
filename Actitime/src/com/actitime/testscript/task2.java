97438fe046979c45d3c6d5112e9f7f5d57910fca
HP <HP@192.168.1.7>
2023-04-15 18:03:38 +0400

    Task version-2

---
+++ Actitime/src/com/actitime/testscript/task2.java
@@ -0,0 +1,14 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class task2 {
+	
+	@Test
+	public void task2version()
+	{
+		Reporter.log("task version 2",true);
+	}
+
+}
